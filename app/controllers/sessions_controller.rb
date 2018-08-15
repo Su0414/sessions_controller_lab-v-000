6b41b76821cd690e658683e6f4db35db95628abf
Suchitra Naik <susanaik@gmail.com>
2018-08-15 02:29:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name] || !params[:name].empty?
+    if params[:name] 
       session[:name] = params[:name]
       redirect_to '/'
     else
